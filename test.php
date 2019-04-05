b963df0b76e647de355c546b0ce659ad090a3ec0
PriscillaStephan <pressistephan@gmail.com>
2019-04-05 16:04:28 +0300

    added test

--- test.php
+++ test.php
@@ -3,55 +3,54 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>SB Admin - Blank Page</title>
+    <title>SB Admin - Blank Page</title>
 
-  <!-- Bootstrap core CSS-->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS-->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-  <!-- Page level plugin CSS-->
-  <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+    <!-- Page level plugin CSS-->
+    <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-  <?php include 'navigations/NavigationBar.php'; ?>
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin.css" rel="stylesheet">
+    <?php include 'navigations/NavigationBar.php'; ?>
 </head>
 
 <div id="content-wrapper">
 
 
 
-  <!-- Breadcrumbs-->
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item">
-      <a href="index.html">Dashboard</a>
-    </li>
-    <li class="breadcrumb-item active">Blank Page</li>
-  </ol>
+    <!-- Breadcrumbs-->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.html">Dashboard</a>
+        </li>
+        <li class="breadcrumb-item active">Blank Page</li>
+    </ol>
 
-  <!-- Page Content -->
-  <h1>Blank Page</h1>
-  <hr>
-  <p>This is a great starting point for new custom pages.</p>
+    <!-- Page Content -->
+    <h1>Blank Page</h1>
+    <hr>
+    <p>This is a great starting point for new custom pages.</p>
 
 </div>
 <!-- /.container-fluid -->
 
 <!-- Sticky Footer -->
 <footer class="sticky-footer">
-  <div class="container my-auto">
-    <div class="copyright text-center my-auto">
-      <span>Copyright © Your Website 2018</span>
-    </div>
-  </div>
+
+
+
+
 </footer>
 
 </div>
@@ -71,4 +70,4 @@
 
 
 
-</html>
+</html> 
